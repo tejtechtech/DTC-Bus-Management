467888db9b60f0789c46ed921a919836df9e199b
tejtechtech <127654520+tejtechtech@users.noreply.github.com>
2024-08-31 10:10:25 +0530

    Create ErrorBoundary.js

---
+++ ErrorBoundary.js
@@ -0,0 +1,31 @@
+ErrorBoundary.js
+
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render shows the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can log the error to an error reporting service here
+    console.error("Error caught by ErrorBoundary: ", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+export default ErrorBoundary;
